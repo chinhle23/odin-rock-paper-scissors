27cb25c75efbf1a13344f2bdb432d36365c94256
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-02-05 05:40:04 -0500

    Complete playRound function

--- javascript.js
+++ javascript.js
@@ -10,4 +10,24 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  let playerString = playerSelection.toLowerCase();
+  let player = playerString.replace(playerString[0],playerString[0].toUpperCase());
+  if (player === computerSelection) {
+    return `Tie! Both selected ${player}`
+  } else if (
+    (player === 'Rock' && computerSelection === 'Paper') ||
+    (player === 'Paper' && computerSelection === 'Scissors') || 
+    (player === 'Scissors' && computerSelection === 'Rock')) {
+    return `You Lose! ${computerSelection} beats ${player}`
+  } else if (
+    (player === 'Rock' && computerSelection === 'Scissors') ||
+    (player === 'Scissors' && computerSelection === 'Paper') ||
+    (player === 'Paper' && computerSelection === 'Rock')) {
+    return `You Win! ${player} beats ${computerSelection}`
+  } 
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
