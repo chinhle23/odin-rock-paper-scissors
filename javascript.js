eef8a4e211846d07e7eac717d074925a3a2a2026
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-02-05 05:01:16 -0500

    Complete getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  switch (randomNumber) {
+    case 1:
+      return 'Rock';
+    case 2:
+      return 'Paper';
+    case 3:
+      return 'Scissors';
+  }
+}
+
+console.log(getComputerChoice());
