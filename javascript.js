93ae127be1ecde123cadb414b387c8af571c1c00
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-02-06 05:33:07 -0500

    Complete playGame function

--- javascript.js
+++ javascript.js
@@ -14,20 +14,51 @@ function playRound(playerSelection, computerSelection) {
   let playerString = playerSelection.toLowerCase();
   let player = playerString.replace(playerString[0],playerString[0].toUpperCase());
   if (player === computerSelection) {
-    return `Tie! Both selected ${player}`
+    return `Tie!`
   } else if (
     (player === 'Rock' && computerSelection === 'Paper') ||
     (player === 'Paper' && computerSelection === 'Scissors') || 
     (player === 'Scissors' && computerSelection === 'Rock')) {
-    return `You Lose! ${computerSelection} beats ${player}`
+    return `You Lose!`
   } else if (
     (player === 'Rock' && computerSelection === 'Scissors') ||
     (player === 'Scissors' && computerSelection === 'Paper') ||
     (player === 'Paper' && computerSelection === 'Rock')) {
-    return `You Win! ${player} beats ${computerSelection}`
-  } 
+    return `You Win!`
+  } else {
+    return `You Lose! Invalid option`
+  }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let i = 0;
+  while (i < 5) {
+    let playerSelection = prompt('rock, paper, or scissors?');
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    if (result === 'Tie!') {
+      console.log(`Tie! Both chose ${computerSelection}`);
+    } else if (result === 'You Lose! Invalid option') {
+      console.log(`You Lose! Invalid option entered`);
+      computerScore++;
+      i++;
+    } else if (result === 'You Lose!') {
+      console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+      computerScore++;
+      i++;
+    } else {
+      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+      playerScore++;
+      i++;
+    }
+  }
+  if (playerScore > computerScore) {
+    console.log(`Game Over. You win, ${playerScore} to ${computerScore}`)
+  } else {
+    console.log(`Game Over. Computer wins, ${computerScore} to ${playerScore}`)
+  }
+}
+
+playGame();
